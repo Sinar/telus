51bc4c35df4a006d750535b45a95164de59d98b0
Mubiin Kimura <kmubiin@users.noreply.github.com>
2017-08-10 16:27:00 +0800

    Add files via upload
    Further testing for #4 can be seen in 'test5-1.py' and 'test5-2.py'. Tried with list of nested dictionaries, combine into single dictionary and output to JSON literal. Early attempt for #5 can be seen in 'test5-3.py'.

---
+++ test5-3.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+
+from flask import Flask
+import json
+import pymongo
+from bson import Binary, Code
+from bson import json_util
+import requests
+
+app = Flask(__name__)
+
+def json_data1():
+    data1 = { "A": [
+               { "id": 1,
+               "person": { "name": "John Doe", "gender": "Male"},
+               "organization": { "name": "Volunteer Team" },
+               "post": { "role": "Participant" }
+               },
+               { "id": 2, "person": {}
+               },
+               { "id": 3, "person": {}
+               }   ] }
+    return data1
+
+def json_data2():
+    data2_file = open('data/test5b.json','r')
+    data2 = json.load(data2_file)
+    return data2
+
+def json_data3():
+    mid = '545e621b5222837c2c05a806'
+    url = 'http://api.popit.sinarproject.org/en/memberships/' + mid
+    print('Get details from {0}...'.format(url))
+    req = requests.get(url)
+    data3 = req.json() # return similar to Popit API
+    return data3
+
+def mongo_setup(json_list):
+    try:
+        client = pymongo.MongoClient()
+        database = client.test
+        collection = database.persons
+        print('Create collection was successful')
+        collection.drop() # empty collection before insert new data
+        collection.insert_many(json_list) # insert collection many at once
+        print('Insert JSON data was successful')
+    except Exception as e:
+        print('Exception: {0}'.format(e))
+    finally:
+        print('Setup has completed')
+    return 0
+
+def mongo_dump():
+    try:
+        client = pymongo.MongoClient()
+        database = client.test
+        print('Find collection from MongoDB BSON')
+        collection = database.persons.find()
+        print('Print data from MongoDB BSON')
+        # need to convert cursor object -> JSON string -> JSON literal
+        print(collection) # <pymongo.cursor.Cursor object at ...>
+        data_list = [] # use an empty list to hold multiple dict
+        for item in collection:
+            print(item) # {u'_id': ObjectId...}
+            parse = json.dumps(item, default=json_util.default)
+            print(type(parse)) # <type 'str'>
+            print(parse) # {u'_id': "$oid"...}
+            parse = json.loads(parse)
+            print(type(parse)) # <type 'dict'>
+            print(parse)
+            data_list.append(parse)
+        print('Preview list of dictionaries')
+        print(type(data_list))
+        print(data_list)
+        print('Put the dictionaries into single dictionary...')
+        data = {} # start with an empty dict
+        data = dict(i for d in data_list for i in d.items())
+        print('Preview JSON literal:')
+        print(type(data)) # <type 'dict'>
+        print(data) # visible u'' strings in terminal emulator only
+    except Exception as e:
+        print('Exception: {0}'.format(e))
+    finally:
+        print('Dump has completed')
+    return data # return as valid JSON literal
+
+@app.route('/')
+@app.route('/home')
+def main():
+    json_list = [json_data1(), json_data2(), json_data3()] # list JSON data
+    print('Input JSON data:')
+    source = dict(i for d in json_list for i in d.items()) # add many items
+    source_pretty = json.dumps(source, indent=4, sort_keys=True)
+    print(source_pretty) # preview input JSON data
+    print('Save JSON data to MongoDB BSON...')
+    mongo_setup(json_list) # create record and save JSON data
+    print('Read JSON data from MongoDB BSON...')
+    result = mongo_dump() # dump from record to JSON data again
+    print('Output JSON data:')
+    result_pretty = json.dumps(result, indent=4, sort_keys=True)
+    print(result_pretty) # output JSON data should appear similar to input
+    result = app.response_class(
+                response = json.dumps(result),
+                status = 200,
+                mimetype = 'application/json')
+    print('Output response:')
+    print(result) # only response status will be printed here
+    return result # return JSON data as object, visible in web browser
+
+if __name__ == '__main__':
+    app.run()
