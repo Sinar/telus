bb29372090c19689337749e1dfcfb56ec84d8665
Mubiin Kimura <kmubiin@users.noreply.github.com>
2017-08-18 16:02:28 +0800

    Add files via upload
    First attempt to provide mock data for "tender" and "award" objects.

---
+++ sample.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+def json_file(path):
+    fopen = open(path,'r')
+    fdata = json.load(fopen)
+    return fdata
+
+@app.route('/')
+def main():
+    return 'Hello Flask'
+
+@app.route('/release/1',methods=['GET'])
+def get_tender():
+    parse = json_file('data/tender.json')
+    tender = app.response_class(
+                response = json.dumps(parse),
+                status = 200,
+                mimetype = 'application/json')
+    return tender
+
+@app.route('/release/2',methods=['GET'])
+def get_award():
+    parse = json_file('data/award.json')
+    tender = app.response_class(
+                response = json.dumps(parse),
+                status = 200,
+                mimetype = 'application/json')
+    return tender
+
+if __name__ == '__main__':
+    app.run()
