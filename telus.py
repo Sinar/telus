c9170df2bf21165ec205a81eb9ecab2767435297
Mubiin Kimura <kmubiin@users.noreply.github.com>
2017-08-25 00:47:43 +0800

    Update telus.py
    Add docstrings to main module and function. Start with inline documentation, with respect to PEP 257 -- Docstring conventions (https://www.python.org/dev/peps/pep-0257/). Be friendly to source code readers.

--- telus.py
+++ telus.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
+"""
+This is the main script for telus.
+
+telus is a project implemented in Python. The codes are developed
+using Python 2.7 to be compatible with most Python frameworks and
+older systems with long term support.
+
+The code syntax shall be as much as compatible with Python 3.
+"""
 
 def main():
+    """The main function and default route for app."""
     msg = 'Hello, telus!'
     print(msg)
 
