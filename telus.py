6aafb2a2bd68e74bb744566f32b5d37cc7592401
Mubiin Kimura <kmubiin@users.noreply.github.com>
2017-08-25 02:17:38 +0800

    Update telus.py
    Add sys module and introduce function to identify Python version and system environment. Helpful one-line message when debugging.

--- telus.py
+++ telus.py
@@ -11,9 +11,19 @@ The code syntax shall be as much as compatible with Python 3.
 
 from __future__ import print_function
 
+import sys
+
+def check_env():
+    """Return Python version and system platform."""
+    xyz, _ = sys.version.split(' ', 1)
+    sps = sys.platform
+    return xyz, sps
+
 def main():
     """The main function and default route for app."""
     print('Hello, telus!')
+    version, platform = check_env()
+    print('Using Python {0} on {1}'.format(version, platform))
 
 if __name__ == '__main__':
     main()
