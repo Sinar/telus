dab136e2c9920bbff8b8a090f59ca8e80d1ea84d
Mubiin Kimura <kmubiin@users.noreply.github.com>
2017-08-25 17:54:52 +0800

    Update telus.py
    Remove functions from main script, which were copied to jsonl.py module in previous commit. Remove unused json module. Add lib.jsonl module and rename function call accordingly.

--- telus.py
+++ telus.py
@@ -14,7 +14,7 @@ from __future__ import print_function
 import sys
 import os
 import fnmatch
-import json
+import lib.jsonl
 
 def check_env():
     """Return Python version and system platform."""
@@ -33,44 +33,6 @@ def list_files(spath, blob):
             slist.append(fname)
     return spath, slist, scount
 
-def test_one(spath, fname):
-    """Test one JSONL file."""
-    try:
-        full_path = spath + '/' + fname
-        print('Read {}'.format(full_path))
-        lines = 0
-        for jobject in open(full_path, 'r'):
-            lines = lines + 1
-            json.loads(jobject)
-        print('Parsed objects: {}'.format(lines))
-    except:
-        print('Unexpected JSON object, check the syntax')
-        raise
-
-def test_many(path, flist):
-    """Test many JSONL files."""
-    try:
-        files = 0
-        for fname in flist:
-            files = files + 1
-            test_one(path, fname)
-        print('Parsed files in total: {}'.format(files))
-    except:
-        print('Bad JSON list file')
-        raise
-
-def scan_jsonl(spath, flist, fcount):
-    """Decide whether to test one or many JSONL files."""
-    if fcount == 1:
-        print('Test the only file')
-        test_one(spath, flist[0])
-    elif fcount > 1:
-        print('Test many files')
-        test_many(spath, flist)
-    else:
-        print('Huh, something was wrong')
-        sys.exit(1)
-
 def main():
     """The main function and default route for app."""
     print('Hello, telus!')
@@ -78,7 +40,7 @@ def main():
     print('Using Python {0} on {1}'.format(version, platform))
     spath, listed, counted = list_files('./data', '*.jsonl')
     print('JSONL files found: {}'.format(counted))
-    scan_jsonl(spath, listed, counted)
+    lib.jsonl.scan_jsonl(spath, listed, counted)
     print('All files contain valid JSON objects')
 
 if __name__ == '__main__':
