70ada4d8604e7f9b30af68de37beac727d5b0d72
Mubiin Kimura <kmubiin@users.noreply.github.com>
2017-08-27 03:57:06 +0800

    Update telus.py
    Remove functions from main script, which were copied to query.py module in previous commit. Remove unused modules. Add lib.query module and rename function calls accordingly.

--- telus.py
+++ telus.py
@@ -12,46 +12,9 @@ The code syntax shall be as much as compatible with Python 3.
 from __future__ import print_function
 
 import sys
-import os
-import fnmatch
+import lib.query
 import lib.jsonl
 import pymongo
-
-def get_version():
-    """Return the version number of Python interpreter."""
-    xyz, _ = sys.version.split(' ', 1)
-    return xyz
-
-def get_platform():
-    """Return the identity of system on which Python is running."""
-    sps = sys.platform
-    return sps
-
-def print_env():
-    """Print information of system environment."""
-    version = get_version()
-    platform = get_platform()
-    print('Using Python {0} on {1}'.format(version, platform))
-
-def list_files(spath, blob):
-    """Return list, count of files with specified type."""
-    slist = []
-    scount = 0
-    for fname in os.listdir(spath):
-        if fnmatch.fnmatch(fname, blob):
-            scount = scount + 1
-            print('Found {}'.format(fname))
-            slist.append(fname)
-    return slist, scount
-
-def list_objects(fpath):
-    """Return list, count of objects from specified file."""
-    slist = []
-    lines = 0
-    for sobject in open(fpath, 'r'):
-        lines = lines + 1
-        slist.append(sobject)
-    return slist, lines
 
 def test_conn():
     """Test connection to MongoDB server."""
@@ -87,8 +50,8 @@ def set_collection(dbname, ccname):
 def main():
     """The main function and default route for app."""
     print('Hello, telus!')
-    print_env()
-    listed, counted = list_files('./data', '*.jsonl')
+    lib.query.print_env()
+    listed, counted = lib.query.list_files('./data', '*.jsonl')
     print('JSONL files found: {}'.format(counted))
     lib.jsonl.scan_jsonl('./data', listed, counted)
     test_conn()
@@ -96,7 +59,8 @@ def main():
     print('Created database: {}'.format(database.name))
     collection = set_collection('telus', 'example')
     print('Created collection: {}'.format(collection.name))
-    listed2, counted2 = list_objects('./data/jkr-keputusan_tender.jsonl')
+    listed2, counted2 = lib.query.list_objects(
+                            './data/jkr-keputusan_tender.jsonl')
     print('Preview counted objects: {}'.format(counted2))
     print('Preview first object: {}'.format(listed2[0]))
 
