209afee144316b6cba8501f32d2554cf1af62c4e
Mubiin Kimura <kmubiin@users.noreply.github.com>
2017-08-29 00:51:09 +0800

    Update telus.py
    Remove functions from main script, which were copied to pymg3.py module in earlier commit. Remove unused modules. Add lib.pymg3 module and rename function calls accordingly.

--- telus.py
+++ telus.py
@@ -11,88 +11,18 @@ The code syntax shall be as much as compatible with Python 3.
 
 from __future__ import print_function
 
-import sys
 import lib.query
 import lib.jsonl
-import pymongo
-import json
-from bson.objectid import ObjectId
-from bson import json_util
-
-def test_conn():
-    """Test connection to MongoDB server."""
-    try:
-        print('Using PyMongo {}'.format(pymongo.version))
-        client = pymongo.MongoClient(
-                    connectTimeoutMS=2000,
-                    serverSelectionTimeoutMS=3000)
-        client.admin.command("ismaster")
-    except pymongo.errors.ConnectionFailure:
-        print('Refused connection, check if server is running')
-        sys.exit(1)
-    else:
-        print('Connected to server')
-
-def use_conn():
-    """Return client for a MongoDB instance."""
-    client = pymongo.MongoClient()
-    return client
-
-def set_database(dbname):
-    """Return database with specified name on MongoDB."""
-    client = use_conn()
-    database = client[dbname]
-    print('Created database: {}'.format(database.name))
-    return database
-
-def set_collection(dbname, ccname):
-    """Return collection with specified name on MongoDB."""
-    client = use_conn()
-    collection = client[dbname][ccname]
-    print('Created collection: {}'.format(collection.name))
-    return collection
-
-def add_test(ccobject, olist):
-    """Test add objects from list into specified collection."""
-    if ccobject.count == 0:
-        pass
-    else:
-        print('Collection was not empty, drop first')
-        ccobject.drop()
-    tcount = 0
-    for sobject in olist:
-        tcount = tcount + 1
-        jobject = json.loads(sobject)
-        result = ccobject.insert_one(jobject)
-        mobjectid = result.inserted_id
-        if tcount == 1:
-            fmoid = mobjectid
-        else:
-            lmoid = mobjectid
-        #print('Inserted object {0}: {1}'.format(tcount, mobjectid))
-    return fmoid, lmoid
-
-def store_awards(fpath):
-    """Store awards from JSONL into MongoDB."""
-    print('Prepare to store awards')
-    collection = set_collection('telus', 'example')
-    listed2, counted2 = lib.query.list_objects(fpath)
-    print('Preview counted objects: {}'.format(counted2))
-    print('Preview first object: {}'.format(listed2[0]))
-    fmoid, _ = add_test(collection, listed2)
-    print('Inserted objects: {}'.format(collection.count()))
-    rdict_fmoid = collection.find_one({'_id':ObjectId(fmoid)})
-    jdict_fmoid = json.dumps(rdict_fmoid, default=json_util.default)
-    print('Inserted first object: {}'.format(jdict_fmoid))
+import lib.pymg3
 
 def main():
     """The main function and default route for app."""
     print('Hello, telus!')
     lib.query.print_env()
     lib.jsonl.scan_jsonl('./data')
-    test_conn()
-    set_database('telus')
-    store_awards('./data/jkr-keputusan_tender.jsonl')
+    lib.pymg3.test_conn()
+    lib.pymg3.set_database('telus')
+    lib.pymg3.store_awards('./data/jkr-keputusan_tender.jsonl')
 
 if __name__ == '__main__':
     main()
